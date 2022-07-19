9e274dcd57d716c8c012e2aecb428968a720ffe6
Shajee98 <s.bin.17878@khi.iba.edu.pk>
2022-07-20 00:17:01 +0500

    second commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@
  * @format
  * @flow strict-local
  */
-
+import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,38 +1,50 @@
-import { Text, TextInput, View } from 'react-native'
+import React from 'react'
+import { KeyboardAvoidingView, ScrollView, Text, TextInput, View } from 'react-native'
 import { useEffect, useLayoutEffect, useState } from 'react';
 import {io} from 'socket.io-client';
 import {useIsFocused} from '@react-navigation/native'
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const ChatScreen = () => {
     const [message, setMessage] = useState('')
     const isFocused = useIsFocused();
+    console.log(isFocused)
     const [chatMessages, setChatMessages] = useState([])
     var socket = io()
 
-    useLayoutEffect(async() => {
+    useLayoutEffect(() => {
+      async function connect() {
         socket = io("https://lis-chat-test.herokuapp.com")
-        setChatMessages(JSON.parse(await AsyncStorage.getItem("chatMessages")))
+      }
+      connect();
     },[])
   
-    useEffect(async() => {
-      socket.on("1", message => {setChatMessages(...chatMessages,message)})
-      await AsyncStorage.setItem("chatMessages", JSON.stringify(chatMessages))
+    useEffect(() => {
+      async function getmessages() {
+      // socket.on("1", ({message}) => {setChatMessages(currentMessages => [...currentMessages,message])})
+      socket.on("1", message => {setChatMessages([...chatMessages,message])})
       if (isFocused === false) {
         setChatMessages([])
       }
-    },[message, isFocused])
+    }
+    getmessages();
+    },[message])
   
     const submitChatMessage = () => {
-      setChatMessages(...chatMessages,message)
+      setChatMessages(currentMessages => [...currentMessages,message])
       socket.emit("2",message);
       setMessage("")
     }
   return (
-    <View style={{flexDirection: "row",alignItems: "flex-end",width: "100%",padding: 10, flex: 1}}>
-    {chatMessages.map((message) => (
-      <Text>{message}</Text>
+    <SafeAreaView style={{flex: 1, backgroundColor: "white"}}>
+    <KeyboardAvoidingView style={{flex: 1,paddingBottom: -20}} keyboardVerticalOffset={-500}>
+      <ScrollView>
+    {chatMessages.map((message, key) => (
+      <Text style={{color: "black",fontWeight: "500",marginLeft: 10,marginBottom: 15}} key={key}>{message}</Text>
     ))}
+    </ScrollView>
+    <View style={{flexDirection: "row",alignItems: "center",width: "100%",padding: 10, bottom: 0}}>
      <TextInput style={{bottom: 0,
      height: 40,
      flex: 1,
@@ -41,7 +53,9 @@ const ChatScreen = () => {
      padding: 10,
      color: "grey",
      borderRadius: 30}} placeholder='Enter message' onSubmitEditing={() => submitChatMessage()} value={message} onChangeText={(text) => setMessage(text)}/>
-   </View>
+     </View>
+   </KeyboardAvoidingView>
+   </SafeAreaView>
   )
 }
 
